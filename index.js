2c614c57150dc840172fd74388a3ddf64de19250
taylor kraft <taylorkraft6@gmail.com>
2021-11-08 21:21:45 -0700

    rides over 2500 foot distance are not allowed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function scuberGreetingForFeet(distance){
   let fee;
   if (distance <= 400) {
     fee = 'This one is on me!'
+  } else if (distance > 2500) {
+    fee = 'No can do.'
   } else if (distance > 2000) {
     fee = 'I will gladly take your thirty bucks.'
   }
